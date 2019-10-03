7111364a75474d64c64d708d4e0a3a939476b1a0
mikerainey <me@mike-rainey.site>
2019-10-03 15:21:56 -0400

    factoring grep example so that our external benchmarking can share it

--- examples/grep.cpp
+++ examples/grep.cpp
@@ -1,8 +1,7 @@
-#include "sequence.h"
 #include "get_time.h"
 #include "strings/string_basics.h"
 #include "parse_command_line.h"
-#include "group_by.h"
+#include "grep.h"
 
 using namespace pbbs;
 
@@ -14,26 +13,12 @@ int main (int argc, char *argv[]) {
 
   timer t("grep", true);
   
-  pbbs::range<char*> str = pbbs::char_range_from_file(filename);
+  range<char*> str = char_range_from_file(filename);
   t.next("read file");
   sequence<char> out_str;
 
   for (int i=0; i < rounds; i++) {
-    // auto foo = pack_index<size_t>(seq(str.size(), [&] (size_t i) {
-    // 	  return str[i] == '\n';}));
-    // size_t l = str.size() - search_str.size() + 1;
-    // auto bar = pack_index<size_t>(seq(l, [&] (size_t i) {
-    // 	  size_t j;
-    // 	  for (j=0; j < search_str.size(); j++)
-    // 	    if (str[i+j] != search_str[j]) break;
-    // 	  return (j == search_str.size());
-    // 	}));
-    auto is_line_break = [&] (char a) {return a == '\n';};
-    auto cr = singleton('\n');
-    auto lines = filter(split(str, is_line_break), [&] (auto const &s) {
-    	return search(s, search_str) < s.size();});
-    out_str = flatten(tabulate(lines.size()*2, [&] (size_t i) {
-    	  return (i & 1) ? cr : std::move(lines[i/2]);}));
+    out_str = grep(str, search_str);
     t.next("do work");
   }
   cout << out_str;
---
+++ examples/grep.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "sequence.h"
+#include "group_by.h"
+
+namespace pbbs {
+
+auto grep(range<char*> str, sequence<char>& search_str) -> sequence<char> {
+  auto is_line_break = [&] (char a) {return a == '\n';};
+  auto cr = singleton('\n');
+  auto lines = filter(split(str, is_line_break), [&] (auto const &s) {
+      return search(s, search_str) < s.size();});
+  auto out_str = flatten(tabulate(lines.size()*2, [&] (size_t i) {
+      return (i & 1) ? cr : std::move(lines[i/2]);}));
+  return out_str;
+}
+
+}
