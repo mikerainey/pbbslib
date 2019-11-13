5b6dee2f3ca2cf34236e87a9d8afb981a3ec4673
Mike Rainey <me@mike-rainey.site>
2019-11-13 15:24:49 -0500

    optimization for grep to eliminate temporary arrays

--- examples/grep.h
+++ examples/grep.h
@@ -8,7 +8,7 @@ namespace pbbs {
 auto grep(range<char*> str, sequence<char>& search_str) -> sequence<char> {
   auto is_line_break = [&] (char a) {return a == '\n';};
   auto cr = singleton('\n');
-  auto lines = filter(split(str, is_line_break), [&] (auto const &s) {
+  auto lines = filter(split_range(str, is_line_break), [&] (auto const &s) {
       return search(s, search_str) < s.size();});
   return flatten(tabulate(lines.size()*2, [&] (size_t i) {
           return (i & 1) ? cr : std::move(lines[i/2]);}));
