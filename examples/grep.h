392459a488aeca364292e4199ff77f5646e54ef3
Mike Rainey <me@mike-rainey.site>
2019-11-13 15:57:52 -0500

    cleanup

--- examples/grep.h
+++ examples/grep.h
@@ -6,12 +6,13 @@
 namespace pbbs {
 
 auto grep(range<char*> str, sequence<char>& search_str) -> sequence<char> {
+  auto is_even = [] (size_t i) { return i & 1; };
   auto is_line_break = [&] (char a) {return a == '\n';};
   auto cr = singleton('\n');
   auto lines = filter(split_range(str, is_line_break), [&] (auto const &s) {
       return search(s, search_str) < s.size();});
   return flatten(tabulate(lines.size()*2, [&] (size_t i) {
-          return (i & 1) ? cr : std::move(lines[i/2]);}));
+      return is_even(i) ? cr : lines[i/2];}));
 }
 
 }
